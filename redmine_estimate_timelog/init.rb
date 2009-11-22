7e1b87edf8572945bdb7b61fae76c947665100cc
toritori0318 <toritori0318@gmail.com>
2009-11-23 01:42:19 +0900

    bugfix

---
+++ redmine_estimate_timelog/init.rb
@@ -0,0 +1,9 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_estimate_timelog do
+  name 'Redmine Estimate Timelog plugin'
+  author 'toritori0318'
+  description 'Plan/results report indication plugin'
+  version '1.0.0'
+  menu :top_menu, :redmine_estimate_timelog, {:controller => 'estimate_timelog', :action => 'report'}, :caption =>   :et_label_menu, :last => true
+end
