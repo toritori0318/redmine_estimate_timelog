590a1e3511a9e6e076024222ebdbf27ae41bf8d4
toritori0318 <toritori0318@gmail.com>
2009-11-29 14:19:40 +0900

    IEのdeバグ修正

--- redmine_estimate_timelog/init.rb
+++ redmine_estimate_timelog/init.rb
@@ -4,6 +4,6 @@ Redmine::Plugin.register :redmine_estimate_timelog do
   name 'Redmine Estimate Timelog plugin'
   author 'toritori0318'
   description 'Plan/results report indication plugin'
-  version '0.1.0'
+  version '0.1.1'
   menu :top_menu, :redmine_estimate_timelog, {:controller => 'estimate_timelog', :action => 'report'}, :caption =>   :et_label_menu, :last => true
 end
