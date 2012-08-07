c59de0969811b6075e1518573a3b93fc2172c6b2
cou2jpn <anemone.reef@gmail.com>
2012-08-07 17:49:57 +0900

    Fix for 1.4.x easily. And modified to work in development mode.

--- redmine_estimate_timelog/init.rb
+++ redmine_estimate_timelog/init.rb
@@ -5,6 +5,6 @@ Redmine::Plugin.register :redmine_estimate_timelog do
   author 'toritori0318'
   description 'This is a plugin for Redmine'
   version '0.4.0'
-  requires_redmine :version_or_higher => '1.3.0'
+  requires_redmine :version_or_higher => '1.4.0'
   menu :top_menu, :redmine_estimate_timelog, {:controller => 'estimate_timelog', :action => 'report'}, :caption => :et_label_menu
 end
