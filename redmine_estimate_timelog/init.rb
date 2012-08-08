051361032241ec96960e10dfdbedd0fc6606d214
cou2jpn <anemone.reef@gmail.com>
2012-08-08 13:20:11 +0900

    Update version.

--- redmine_estimate_timelog/init.rb
+++ redmine_estimate_timelog/init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_estimate_timelog do
   name 'Redmine Estimate Timelog plugin'
   author 'toritori0318'
   description 'This is a plugin for Redmine'
-  version '0.4.0'
+  version '0.4.1'
   requires_redmine :version_or_higher => '1.4.0'
   menu :top_menu, :redmine_estimate_timelog, {:controller => 'estimate_timelog', :action => 'report'}, :caption => :et_label_menu
 end
