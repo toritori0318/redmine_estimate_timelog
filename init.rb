f27db114130197aaa66128152448423147800236
toritori0318 <toritori0318@gmail.com>
2016-06-27 01:52:40 +0900

    bump 0.6.0

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_estimate_timelog do
   name 'Redmine Estimate Timelog plugin'
   author 'toritori0318'
   description 'This is a plugin for Redmine'
-  version '0.5.2'
-  requires_redmine :version_or_higher => '2.0.0'
+  version '0.6.0'
+  requires_redmine :version_or_higher => '3.0.0'
   menu :top_menu, :redmine_estimate_timelog, {:controller => 'estimate_timelog', :action => 'report'}, :caption => :et_label_menu
 end
